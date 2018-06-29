cb493f7cd382da4e5c9c0f5d4b95d2fdd0f4b565
Doug Bennett <dben89x@gmail.com>
2018-06-29 14:58:42 -0700

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'pages/home'
+  root to: 'pages#home'
 
-  get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
