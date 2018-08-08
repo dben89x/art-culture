0a418dade8b2d5ee66db54f34c39693c89783b5b
Doug Bennett <dben89x@gmail.com>
2018-08-08 01:39:59 -0600

    stuff

--- app/mailers/bid_mailer.rb
+++ app/mailers/bid_mailer.rb
@@ -1,21 +1,21 @@
 class BidMailer < ApplicationMailer
 
   def created_email(bid, seller)
-    unless Rails.env.development?
+    unless Rails.env.development? || Rails.env.test?
       @bid = bid
       mail(to: seller, subject: "New bid on #{@bid.artwork.title} from #{@bid.user.full_name}")
     end
   end
 
   def accepted_email(bid, buyer)
-    unless Rails.env.development?
+    unless Rails.env.development? || Rails.env.test?
       @bid = bid
       mail(to: buyer, subject: "Your bid on #{@bid.artwork.title} has been accepted!")
     end
   end
 
   def paid_email(artwork_log, seller)
-    unless Rails.env.development?
+    unless Rails.env.development? || Rails.env.test?
       @artwork_log = artwork_log
       mail(to: seller, subject: "Payment of #{@artwork_log.price} for #{@artwork_log.artwork.title}!")
     end
