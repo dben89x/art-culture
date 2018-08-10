2a716cc21106db11def1ae7c1ed5c4a0d531b0b4
Doug Bennett <dben89x@gmail.com>
2018-08-10 14:51:29 -0600

    .

--- app/models/artwork_image.rb
+++ app/models/artwork_image.rb
@@ -16,9 +16,9 @@ class ArtworkImage < ApplicationRecord
   validate :is_within_limit
 
   attr_accessor :url, :url_cache
-  mount_uploader :url, BlogImageUploader if Rails.env.production?
+  mount_uploader :url, ArtworkImageUploader
 
-  
+
   def is_within_limit
     unless self.artwork.has_room
       errors.add(:base, "Artwork has too many images")
